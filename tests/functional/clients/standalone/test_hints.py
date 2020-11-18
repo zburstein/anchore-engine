cc99094806cfd84e457546d0288b87c8b2640c8e
Toure Dunnon <toure.dunnon@anchore.com>
2020-11-17 20:22:54 -0500

    added a couple more test for rpms and dpkgs.

--- tests/functional/clients/standalone/test_hints.py
+++ tests/functional/clients/standalone/test_hints.py
@@ -1,17 +1,16 @@
 
-
 class TestHints:
-
-    def test_npm(self, hints_image):
+    def test_npm_hints(self, hints_image):
         hints = {
             "packages": [
-                {"name": "safe-buffer",
-                "license": "FREE-FOR-ALL",
-                "version": "100",
-                "type": "npm"}
+                {
+                    "name": "safe-buffer",
+                    "license": "FREE-FOR-ALL",
+                    "version": "100",
+                    "type": "npm"
+                }
             ]
         }
-
         result = hints_image(hints, 'npm')
         pkgs = result['image']['imagedata']['analysis_report']['package_list']['pkgs.npms']['base']
         path = "/usr/lib/node_modules/npm/node_modules/string_decoder/node_modules/safe-buffer/package.json"
@@ -20,3 +19,42 @@ class TestHints:
         assert package['license'] == 'FREE-FOR-ALL'
         assert package['version'] == '100'
         assert package['sourcepkg'] == 'git://github.com/feross/safe-buffer.git'
+
+    def test_rpm_hints(self, hints_image):
+        hints = {
+            "packages": [
+                {
+                    "name": "tar",
+                    "license": "MIT",
+                    "version": "42",
+                    "type": "rpm",
+                    "origin": "Centos"
+                }
+            ]
+        }
+        result = hints_image(hints, 'rpm')
+        pkgs = result['image']['imagedata']['analysis_report']['package_list']['pkgs.allinfo']['base']
+        package = pkgs.get('tar')
+        assert package['type'] == "rpm"
+        assert package['license'] == 'MIT'
+        assert package['version'] == '42'
+        assert package['origin'] == 'Centos'
+
+    def test_dpkg_hints(self, hints_image):
+        hints = {
+            "packages": [
+                {
+                    "name": "adduser",
+                    "version": "43",
+                    "license": "GPL",
+                    "type": "dpkg",
+                }
+            ]
+        }
+        result = hints_image(hints, 'stretch-slim')
+        pkgs = result['image']['imagedata']['analysis_report']['package_list']['pkgs.allinfo']['base']
+        package = pkgs.get('adduser')
+        assert package['type'] == "dpkg"
+        assert package['version'] == '43'
+        assert package['license'] == 'GPL'
+
